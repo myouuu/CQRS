5271240ffd3d24b35cf77c756d7fdb07af329f54
Myouuu <meriem.mrabent@hetic.net>
2020-09-11 11:37:58 +0200

    archi

---
+++ db/respository.go
@@ -0,0 +1,30 @@
+package db
+import (
+	"context"
+
+	"github.com/myouuu/CQRS/schema"
+)
+
+type Repository interface {
+	Close()
+	InsertMeow(ctx context.Context, meow schema.CQRS) error
+	ListMeows(ctx context.Context, skip uint64, take uint64) ([]schema.CQRS, error)
+}
+
+var impl Repository
+
+func SetRepository(repository Repository) {
+	impl = repository
+}
+
+func Close() {
+	impl.Close()
+}
+
+func InsertMeow(ctx context.Context, meow schema.CQRS) error {
+	return impl.InsertMeow(ctx, meow)
+}
+
+func ListMeows(ctx context.Context, skip uint64, take uint64) ([]schema.CQRS, error) {
+	return impl.ListMeows(ctx, skip, take)
+}
